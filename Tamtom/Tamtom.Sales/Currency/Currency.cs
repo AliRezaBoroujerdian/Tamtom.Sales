c8d0d7d6df7b8f82d6ddbdfa81399f3ed8badb49
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2020-03-01 23:53:44 +0330

    Add currency

---
+++ Tamtom/Tamtom.Sales/Currency/Currency.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tamtom.Database.Dapper.Crud;
+using static Tamtom.Database.Dapper.Crud.CrudModels;
+using static Tamtom.Database.Dapper.Generic;
+
+namespace Tamtom.Sales.Currency
+{
+    public class Currency : Crud, ICrud
+    {
+        public Currency() : base("Currency", "Sales") { }
+
+        public async Task<CurrencyModel> UserCurrency(SelectSingleModelWithID model)
+        {
+            return await ExecuteStoredProcedureFirstOrDefaultAsync<SelectSingleModelWithID, CurrencyModel>("[Identity].APP_SP_SEL_CurrencyByUserID", model);
+        }
+
+        #region Not implemented method
+        public Task<int> DeActive(CrudModels.PrimaryKeyID id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeActive(CrudModels.PrimaryKeyGuid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeActiveLanguage(CrudModels.DeActiveLanguageModelWithID model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeActiveLanguage(CrudModels.DeActiveLanguageModelWithGuid model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(CrudModels.PrimaryKeyID id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(CrudModels.PrimaryKeyGuid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteLanguage(CrudModels.DeleteLanguageModelWithID model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteLanguage(CrudModels.DeleteLanguageModelWithGuid model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Insert<InputType>(InputType model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertAndReturnInsertedID<InputType>(InputType model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ReturnType>> Select<ReturnType>(CrudModels.SelectModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ReturnType> SelectSingle<ReturnType>(CrudModels.SelectSingleModelWithID model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ReturnType> SelectSingle<ReturnType>(CrudModels.SelectSingleModelWithGuid model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update<InputType>(InputType model)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Tamtom/Tamtom.Sales/Currency/CurrencyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamtom.Sales.Currency
+{
+    public class CurrencyModel
+    {
+        public int CurrencyID { get; set; }
+        public string Symbol { get; set; }
+        public string CurrencyCode { get; set; }
+        public bool? CurrencyIsActive { get; set; }
+        public string Name { get; set; }
+        public bool? LanguageIsActive { get; set; }
+    }
+}
